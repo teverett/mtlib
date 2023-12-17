b4f55119578c0f48d8a2d32cb4058c222428a834
Tom Everett <tom@khubla.com>
2023-12-16 19:16:18 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# mtlib
+
+A Java library for reading and writing Minetest Redis databases
+
---
+++ src/test/java/com/khubla/mtlib/Test1.java
@@ -0,0 +1,13 @@
+package com.khubla.mtlib;
+
+import org.junit.jupiter.api.Test;
+
+public class Test1 {
+   @Test
+   public void testRead() {
+      try {
+      } catch (final Exception e) {
+         e.printStackTrace();
+      }
+   }
+}
