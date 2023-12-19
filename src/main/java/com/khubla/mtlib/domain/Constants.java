bcd6391adc6cf74a8733362c10b60674ef5bb123
Tom Everett <tom@khubla.com>
2023-12-19 12:32:38 -0700

    more

--- src/main/java/com/khubla/mtlib/domain/Constants.java
+++ src/main/java/com/khubla/mtlib/domain/Constants.java
@@ -1,7 +1,8 @@
 package com.khubla.mtlib.domain;
 
 public class Constants {
-   public final static int NODES_PER_BLOCK = 4096; // 16*16*16
+   public final static int BLOCK_SIZE = 16;
+   public final static int NODES_PER_BLOCK = BLOCK_SIZE * BLOCK_SIZE * BLOCK_SIZE; // 16*16*16
    public static final long MAX_EXTENT = 30927;
    public static final long MIN_EXTENT = -30912;
 }
