e8761b9d5ed5f94fc9f8a1e8611ab48994484b3a
Tom Everett <tom@khubla.com>
2023-12-20 10:48:56 -0700

    blocktypes

---
+++ src/main/java/com/khubla/mtlib/map/BlockTypes.java
@@ -0,0 +1,66 @@
+package com.khubla.mtlib.map;
+
+import com.opencsv.CSVReader;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.List;
+
+public class BlockTypes {
+   private static final String RESOURCE_FILE = "/blocktypes.txt";
+   private static BlockTypes instance;
+   private final HashMap<Short, String> blockTypesById = new HashMap<Short, String>();
+   private final HashMap<String, Short> blockTypesByName = new HashMap<String, Short>();
+
+   private BlockTypes() {
+      load();
+   }
+
+   public static BlockTypes getInstance() {
+      if (null == instance) {
+         instance = new BlockTypes();
+      }
+      return instance;
+   }
+
+   public HashMap<Short, String> getBlockTypesById() {
+      return blockTypesById;
+   }
+
+   public HashMap<String, Short> getBlockTypesByName() {
+      return blockTypesByName;
+   }
+
+   private void load() {
+      try {
+         InputStream is = BlockTypes.class.getResourceAsStream(RESOURCE_FILE);
+         List<String[]> data = null;
+         if (null != is) {
+            InputStreamReader inputStreamReader = new InputStreamReader(is);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            try (Reader reader = bufferedReader) {
+               try (CSVReader csvReader = new CSVReader(reader)) {
+                  data = csvReader.readAll();
+               }
+            }
+            if (null != data) {
+               for (String[] line : data) {
+                  int l = parseHex((line[0]));
+                  blockTypesById.put((short) l, line[1].trim());
+                  blockTypesByName.put(line[1].trim(), (short) l);
+               }
+            }
+         }
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+
+   int parseHex(String hex) {
+      int i = hex.indexOf("x");
+      return Integer.parseInt(hex.substring(i + 1).trim(), 16);
+   }
+}
---
+++ src/test/java/com/khubla/mtlib/TestBlockTypes.java
@@ -0,0 +1,18 @@
+package com.khubla.mtlib;
+
+import com.khubla.mtlib.map.BlockTypes;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class TestBlockTypes {
+   @Test
+   public void restBlockTypes() {
+      try {
+         BlockTypes blockTypes = BlockTypes.getInstance();
+         assertNotNull(blockTypes);
+      } catch (final Exception e) {
+         e.printStackTrace();
+      }
+   }
+}
