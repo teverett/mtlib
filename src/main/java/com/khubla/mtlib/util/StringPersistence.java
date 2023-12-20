fa90a9243a66ea21326225aad72f17d3bc42f3d0
Tom Everett <tom@khubla.com>
2023-12-20 10:57:32 -0700

    more

--- src/main/java/com/khubla/mtlib/util/StringPersistence.java
+++ src/main/java/com/khubla/mtlib/util/StringPersistence.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class StringPersistence {
    /**
-    * read a null terminated string
+    * read a pascal string with a 16 bit length byte
     */
    public static String read16(DataInputStream dis) throws IOException {
       short string_len = dis.readShort();
@@ -15,6 +15,9 @@ public class StringPersistence {
       return new String(string);
    }
 
+   /**
+    * read a pascal string with a 32 bit length byte
+    */
    public static String read32(DataInputStream dis) throws IOException {
       int string_len = dis.readInt();
       byte[] string = new byte[string_len];
@@ -22,16 +25,25 @@ public class StringPersistence {
       return new String(string);
    }
 
+   /**
+    * write a pascal string with a 16 bit length byte
+    */
    public static void write16(DataOutputStream dos, String str) throws IOException {
       dos.writeShort(str.length());
       dos.write(str.getBytes(), 0, str.length());
    }
 
+   /**
+    * write a pascal string with a 32 bit length byte
+    */
    public static void write32(DataOutputStream dos, String str) throws IOException {
       dos.writeInt(str.length());
       dos.write(str.getBytes(), 0, str.length());
    }
 
+   /**
+    * read a null terminated string
+    */
    public static String readSz(DataInputStream dis) throws IOException {
       StringBuilder sb = new StringBuilder();
       byte b = dis.readByte();
